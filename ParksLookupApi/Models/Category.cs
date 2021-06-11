767d5726130d79c403b60a966baa89e8ac545468
Vanessa Su <vnessa.su@gmail.com>
2021-06-11 12:40:32 -0400

    :hammer: Add Category model

---
+++ ParksLookupApi/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace ParksLookupApi.Models
+{
+  public class Category
+  {
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+  }
+}
