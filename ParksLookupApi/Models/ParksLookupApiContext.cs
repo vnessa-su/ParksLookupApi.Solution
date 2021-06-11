a1afaba252d831f4bfe493ed26c2571803099b09
Vanessa Su <vnessa.su@gmail.com>
2021-06-11 12:50:14 -0400

    :hammer: Add ParksLookupApiContext model

---
+++ ParksLookupApi/Models/ParksLookupApiContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksLookupApi.Models
+{
+  public class ParksLookupApiContext : DbContext
+  {
+    public ParksLookupApiContext(DbContextOptions<ParksLookupApiContext> options) : base(options) { }
+
+    public DbSet<Park> Parks { get; set; }
+    public DbSet<Category> Categories { get; set; }
+  }
+}
