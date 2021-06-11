a6018d8c083a506951017d5a11309fbac1e7592e
Vanessa Su <vnessa.su@gmail.com>
2021-06-11 14:21:49 -0400

    :hammer: Add CRUD routes to Parks controller

--- ParksLookupApi/Controllers/ParksController.cs
+++ ParksLookupApi/Controllers/ParksController.cs
@@ -20,11 +20,84 @@ namespace ParksLookupApi.Controllers
       _db = db;
     }
 
-    // GET: parks
+    //GET: parks
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Park>>> Get()
     {
       return await _db.Parks.ToListAsync();
+    }
+
+    //POST: parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+
+    //GET: parks/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      Park selectedPark = await _db.Parks.FindAsync(id);
+      if (selectedPark == null)
+      {
+        return NotFound();
+      }
+
+      return selectedPark;
+    }
+
+    // PUT: parks/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return Ok(park);
+    }
+
+    //DELETE: parks/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      Park selectedPark = await _db.Parks.FindAsync(id);
+      if (selectedPark == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(selectedPark);
+      await _db.SaveChangesAsync();
+
+      return Ok();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
     }
   }
 }
